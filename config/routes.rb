fb759e2c721b38c67a1da51ed83e0c4cf45e13c6
jwqdjs <jwqdjsjwqdjs@gmail.com>
2024-10-20 07:08:54 +0800

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # API definitions
   namespace :api, defaults: {format: :json} do
     # we are going to list our resources here
+      namespace :v1 do
 
+      end
   end
 end
