d4a2d79abfa0e5df86e0ce59c5765ba5cf0271b5
jwqdjs <jwqdjsjwqdjs@gmail.com>
2024-10-23 18:44:30 +0800

    Adds order confirmation mailer

---
+++ app/models/order.rb
@@ -0,0 +1,13 @@
+class Order < ApplicationRecord
+  belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :products, through: :placements
+
+  validates :total, numericality: { greater_than_or_equal_to: 0 }
+  validates :total, presence: true 
+  before_validation :set_total!
+
+  def set_total!
+    self.total = products.sum :price
+  end
+end
